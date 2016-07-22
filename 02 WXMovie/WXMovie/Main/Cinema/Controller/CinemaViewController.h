16133a1820be33ea547e6df02209c2f5c3233c1a
月夜雨墨 <seeea@SeeeadeMacBook-Pro.local>
2016-07-22 12:29:03 +0800

    did i success?
    yes!

---
+++ 02 WXMovie/WXMovie/Main/Cinema/Controller/CinemaViewController.h
@@ -0,0 +1,13 @@
+//
+//  CinemaViewController.h
+//  WXMovieZZJ
+//
+//  Created by zhongzhongjun on 16/4/17.
+//  Copyright © 2016年 wxhl. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+@interface CinemaViewController : BaseViewController
+
+@end
---
+++ 02 WXMovie/WXMovie/Main/More/Controller/MoreViewController.h
@@ -0,0 +1,13 @@
+//
+//  MoreViewController.h
+//  WXMovieZZJ
+//
+//  Created by zhongzhongjun on 16/4/17.
+//  Copyright © 2016年 wxhl. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+@interface MoreViewController : BaseViewController
+
+@end
